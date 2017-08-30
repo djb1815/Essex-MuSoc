7de6f3707e54c496fe08287b162bbba7be0f18c8
Rjthompson84 <robajob84@gmail.com>
2017-08-30 15:05:06 +0100

    regex fixed
    see line 7 of urls.py, calendar.html changes cosmetic

--- musoc_web/schedule/urls.py
+++ musoc_web/schedule/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.calendar, name='calendar'),
+    url(r'cal/', views.calendar, name='calendar'),
 
 ]
